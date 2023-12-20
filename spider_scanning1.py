227c4205102c1968fe31cb5a890e110bcc06e05c
Henley <53051768+fireun@users.noreply.github.com>
2023-12-20 12:32:20 +0800

    version_3
    based on version 3 already done for spider scanning, but if website got too much sublink it will error like ERROR:__main__:Error connecting to ZAP: HTTPConnectionPool(host='127.0.0.1', port=8080): Max retries exceeded with url
    
    
    So now we can filter if the the sublink too much can add to GetURL3.py to filter URL.

---
+++ spider_scanning1.py
@@ -0,0 +1,52 @@
+# scanning.py
+
+#!/usr/bin/env python
+import time
+#import requests  # Import the requests library
+from zapv2 import ZAPv2
+
+def process_urls(url):
+    # The URL of the application to be tested
+    target = url
+    # Change to match the API key set in ZAP, or use None if the API key is disabled
+    apiKey = 'ddueupb2fk04jfr4b22pc9n6qk'
+
+    # By default ZAP API client will connect to port 8080
+    ##zap = ZAPv2(apikey=apiKey)
+    # Use the line below if ZAP is not listening on port 8080, for example, if listening on port 8090
+    #zap = ZAPv2(apikey=apiKey, proxies={'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'})
+    # Increase the number of retries
+    #zap = ZAPv2(apikey=apiKey, proxies={'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'})
+    
+    # Change this line in scanning.py
+    zap = ZAPv2(apikey=apiKey, proxies={'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'})
+
+    # Add the following line to set a timeout for requests made by ZAP
+    #zap.requests_timeout = 10  # Use an appropriate value for the timeout
+
+    print('Spidering target {}'.format(target))
+    # The scan returns a scan id to support concurrent scanning
+    scanID = zap.spider.scan(target)
+    while int(zap.spider.status(scanID)) < 100:
+        # Poll the status until it completes
+        print('Spider progress %: {}'.format(zap.spider.status(scanID)))
+        time.sleep(1)
+
+    print('Spider has completed!')
+    # Prints the URLs the spider has crawled
+    print('\n'.join(map(str, zap.spider.results(scanID))))
+    #spider_results = '\n'.join(map(str, zap.spider.results(scanID)))
+
+    # Retrieve and return spider results
+    spider_results = zap.spider.results(scanID)
+    #return spider_results #pass bk the python main function
+    return scanID, spider_results
+
+    # If required post-process the spider results
+     # Send the results to a PHP script
+    #send_results_to_php(spider_results)
+
+    # TODO: Explore the Application more with Ajax Spider or Start scanning the application for vulnerabilities
+
+
+
